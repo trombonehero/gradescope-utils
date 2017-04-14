49581e5e45fc89273fe92ff5300f95615002ecd9
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2017-04-14 16:00:57 -0700

    Add visibility decorator

--- gradescope_utils/autograder_utils/decorators.py
+++ gradescope_utils/autograder_utils/decorators.py
@@ -10,6 +10,25 @@ class weight(object):
         func.__weight__ = self.val
         return func
 
+class visibility(object):
+    """Simple decorator to add a __visibility__ property to a function
+
+    Usage: @visibility("hidden")
+
+    Options for the visibility field are as follows:
+
+    - `hidden`: test case will never be shown to students
+    - `after_due_date`: test case will be shown after the assignment's due date has passed
+    - `after_published`: test case will be shown only when the assignment is explicitly published from the "Review Grades" page
+    - `visible` (default): test case will always be shown
+    """
+
+    def __init__(self, val):
+        self.val = val
+
+    def __call__(self, func):
+        func.__visibility__ = self.val
+        return func
 
 class tags(object):
     """Simple decorator to add a __tags__ property to a function
